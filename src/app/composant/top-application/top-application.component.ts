6e73a14d793663ad363342849f645837bdb848a4
nbena69 <naelbenaissapro@gmail.com>
2024-03-11 18:16:57 +0100

    Top app fait

--- src/app/composant/top-application/top-application.component.ts
+++ src/app/composant/top-application/top-application.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {AllService} from "../../service/all.service";
+import {AsyncPipe, CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-top-application',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe, CommonModule
+  ],
   templateUrl: './top-application.component.html',
   styleUrl: './top-application.component.css'
 })
 export class TopApplicationComponent {
+  constructor(private all: AllService) {
+    this.all.chargerListeFrais();
+  }
 
+  getTop() {
+    return this.all.appels_termines;
+  }
 }
--- src/app/service/all.service.ts
+++ src/app/service/all.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {LigneFacturation} from "../metier/ligne-facturation"; // Assurez-vous d'avoir le bon chemin vers votre service GsbApi
 
 @Injectable({
   providedIn: 'root'
 })
 export class AllService {
+  private apiUrl = 'http://localhost/benaissa/ProjetDcsApi/public/api'; // Remplacez cela par l'URL réelle de votre API Laravel
+  private ligneFacturation: LigneFacturation = new LigneFacturation;
+  private _reponses = new BehaviorSubject<LigneFacturation[]>([]);
+  readonly appels_termines = this._reponses.asObservable();
+  public LigneFacturation: LigneFacturation[] = [];
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  get listeFraisObs(): Observable<LigneFacturation[]> {
+    return this._reponses.asObservable();
+  }
+
+  chargerListeFrais() {
+    this.http.get<LigneFacturation[]>(`${this.apiUrl}/one`, )
+      .subscribe(
+        data => {
+          this.LigneFacturation = data;
+          this._reponses.next(this.LigneFacturation);
+          console.log(data);
+        },
+        error => {
+          console.log("Erreur Appel API liste frais", error);
+        }
+      );
+  }
 }
