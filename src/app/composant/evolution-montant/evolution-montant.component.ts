a05174dcd9cd1e3b14ed33c1a0eac0e6226c995e
nbena69 <naelbenaissapro@gmail.com>
2024-03-11 21:28:18 +0100

    2 en cours

--- src/app/composant/evolution-montant/evolution-montant.component.ts
+++ src/app/composant/evolution-montant/evolution-montant.component.ts
@@ -1,21 +1,49 @@
 import {Component} from '@angular/core';
 import {AllService} from "../../service/all.service";
 import {AsyncPipe, CommonModule} from "@angular/common";
+import {Chart, ChartModule} from 'angular-highcharts';
+import {MenuComponent} from "../all/menu/menu.component";
 
 @Component({
   selector: 'app-evolution-montant',
   standalone: true,
   imports: [
-    AsyncPipe, CommonModule
+    AsyncPipe, CommonModule, ChartModule, MenuComponent
   ],
-  templateUrl: './evolution-montant.component.html',
+  template:
+    "<div class=\"charts\">\n" +
+    "\n" +
+    "  <div class=\"line\" [chart]=\"lineChart\">\n" +
+    "\n" +
+    "  </div>\n" +
+    "</div>",
   styleUrls: ['./evolution-montant.component.css']
 })
+
 export class EvolutionMontantComponent {
   constructor(private all: AllService) {
     this.all.evolMontant();
   }
+
   getEvol() {
     return this.all.appels_terminesMontantParMois;
   }
+
+  lineChart=new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Evolution des montants'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Euros €',
+        data: [10, 2, 3,6,9,17,20,10,5,2,16]
+      } as any
+    ]
+  })
 }
--- src/app/composant/evolution-produit/evolution-produit.component.ts
+++ src/app/composant/evolution-produit/evolution-produit.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import {Chart, ChartModule} from "angular-highcharts";
+import {AsyncPipe, CommonModule} from "@angular/common";
+import {MenuComponent} from "../all/menu/menu.component";
 
 @Component({
   selector: 'app-evolution-produit',
   standalone: true,
-  imports: [],
-  templateUrl: './evolution-produit.component.html',
+  imports: [    AsyncPipe, CommonModule, ChartModule, MenuComponent
+  ],
+  template: "<div class=\"charts\">\n" +
+    "  <div class=\"pie\" [chart]=\"pieChart\">\n" +
+    "  </div>\n" +
+    "</div>",
   styleUrl: './evolution-produit.component.css'
 })
 export class EvolutionProduitComponent {
 
+  pieChart=new Chart({
+    chart: {
+      type: 'pie',
+      plotShadow: false,
+    },
+
+    credits: {
+      enabled: false,
+    },
+
+    plotOptions: {
+      pie: {
+        innerSize: '99%',
+        borderWidth: 10,
+        borderColor: '',
+        slicedOffset: 10,
+        dataLabels: {
+          connectorWidth: 0,
+        },
+      },
+    },
+
+    title: {
+      verticalAlign: 'middle',
+      floating: true,
+      text: 'Diseases',
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        type: 'pie',
+        data: [
+          { name: 'COVID 19', y: 1, color: '#eeeeee' },
+
+          { name: 'HIV/AIDS', y: 2, color: '#393e46' },
+
+          { name: 'EBOLA', y: 3, color: '#00adb5' },
+          { name: 'DISPORA', y: 4, color: '#eeeeee' },
+          { name: 'DIABETES', y: 5, color: '#506ef9' },
+        ],
+      },
+    ],
+  })
 }
